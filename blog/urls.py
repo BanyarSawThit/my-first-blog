fe3cf8d31a3948f2cd385ea9035b8f52535ff0a8
BanyarSawThit <banyarsawthit1922017@gmail.com>
2025-02-08 21:01:28 +0800

    added data from the databse.

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path('', views.post_list, name='post_list'),
-    path('', views.test_page, name='test_page'),
+    path('', views.post_list, name='post_list'),
+    # path('', views.test_page, name='test_page'),
 ]
